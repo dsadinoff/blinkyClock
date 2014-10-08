efd0012aca9843ac93a0b8e022146ac377544db0
dsadinoff <danny-github@sadinoff.com>
2014-10-08 13:34:53 +0300

    doc fixes, indentation

--- blinkyClock.ino
+++ blinkyClock.ino
@@ -1,11 +1,18 @@
 /*
 A simple clock.
-
-hour is red
-min is green 
-sec is blue
-/60 sec is white
-*/
+ 
+ hour is red
+ min is green 
+ sec is blue
+ 1/60 sec is white
+ 
+ Click button to advance one minute.
+ Hold button to advance one hour.
+ 
+ MIT License,
+ Danny Sadinoff
+ danny@sadinoff.com
+ */
 #include "FastLED.h" // http://fastled.io/
 #include <Button.h> // http://github.com/virgildisgr4ce/Button
 
@@ -29,45 +36,46 @@ void onPress(Button& b) {
 }
 
 void onHold(Button &b){
-    //advance 1 hour
+  //advance 1 hour
   epoch +=  60 * 60L *1000L;
 }
 
 
 void setup() {
-      
-	// sanity check delay - allows reprogramming if accidently blowing power w/leds
-//   	delay(2000);
-        button.pressHandler(onPress);
-        button.holdHandler(onHold, 1000); /*hold time */
 
-   	// For safety (to prevent too high of a power draw), the test case defaults to
-   	// setting brightness to 25% brightness
-   	LEDS.setBrightness(64);
-        LEDS.addLeds<WS2812B, LED_DATA_LINE, GRB>(leds, NUM_LEDS);
+  // sanity check delay - allows reprogramming if accidently blowing power w/leds
+  //   	delay(2000);
+  button.pressHandler(onPress);
+  button.holdHandler(onHold, 1000); /*hold time */
+
+  // For safety (to prevent too high of a power draw), the test case defaults to
+  // setting brightness to 25% brightness
+  LEDS.setBrightness(64);
+  LEDS.addLeds<WS2812B, LED_DATA_LINE, GRB>(leds, NUM_LEDS);
 }
 
 void loop() {
-        if( stopped){
-          return;
-        } 
-        long now = millis();
-        long curTime = now +epoch;
-        long unixTime = curTime / 1000;
-        int seconds = unixTime % 60;
-        int hours = ((unixTime - (long)seconds ) % (3600L * 12L) )/ 3600L;
-        int minutes = ((unixTime - seconds ) % 3600) /60;
-        int sixtieths = (curTime * 60L / 1000L) %(60) ;
+  if( stopped){
+    return;
+  } 
+  long blinkyMillis = millis();
+  long curTime = blinkyMillis +epoch;  //not really but whatever.
+  long unixTime = curTime / 1000;
+  int seconds = unixTime % 60;
+  int hours = ((unixTime - (long)seconds ) % (3600L * 12L) )/ 3600L;
+  int minutes = ((unixTime - seconds ) % 3600) /60;
+  int sixtieths = (curTime * 60L / 1000L) %(60) ;
 
-	memset(leds, 0,  NUM_LEDS * sizeof(struct CRGB)); //clear all
+  memset(leds, 0,  NUM_LEDS * sizeof(struct CRGB)); //clear all
 
-        leds[sixtieths] = CRGB(20,20,20);
-        leds[hours].r = 64;
-        leds[minutes].g = 64;
-        leds[seconds].b = 64;
-            
-			// and now, show your led array! 
-         LEDS.show();
-         button.process();
+  leds[sixtieths] = CRGB(20,20,20);
+  leds[hours].r = 64;
+  leds[minutes].g = 64;
+  leds[seconds].b = 64;
+
+  // and now, show your led array! 
+  LEDS.show();
+  button.process();
 }
+
 
